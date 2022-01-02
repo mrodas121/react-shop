7793e969780203ef4b38374bd9f5580ce7b33b17
mrodas121 <manuel.rodas121@gmail.com>
2022-01-01 20:21:53 -0600

    inicio en el repo remoto

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Layout from '../containers/Layout';
+import Login from '../containers/Login';
+import RecoveryPassword from '../containers/RecoveryPassword';
+
+import '../styles/global.css';
+
+const App = () => {
+	return (
+		<Layout>
+			<Login />
+			<RecoveryPassword />
+		</Layout>
+	);
+}
+export default App;
