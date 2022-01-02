8ea88f6140ba6f9916b5c8173642abde67060a21
mrodas121 <manuel.rodas121@gmail.com>
2022-01-01 20:47:30 -0600

    manejo de rutas

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname, 'dist'),
 		filename: 'bundle.js',
+		publicPath: '/',
 	},
 	mode: 'development',
 	resolve: {
@@ -51,5 +52,6 @@ module.exports = {
         static: path.join(__dirname,'dist'),
         port: 3111,
         compress: true,
+		historyApiFallback: true,
     }
 }
